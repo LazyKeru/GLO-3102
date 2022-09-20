3668bc6bf038a4352aede9555c33c1817aea2c90
LazyKeru <killian.allaire@hotmail.com>
2022-09-20 17:43:24 -0400

    feat: fading notification

--- laboratoire3/notification.js
+++ laboratoire3/notification.js
@@ -7,11 +7,27 @@ export class Notification {
 
     createNotification = (parentElement) => {
         this.element = parentElement.appendChild(this.HTMLcomponent());
-        setTimeout(() => this.element.remove(),5000);
+        this.coolDownNotification();
     }
 
     coolDownNotification = () => {
-        setTimeout(() => this.element.remove(),5000);
+        setTimeout(()=>this.deleteNotification(),3000)
+    }
+
+    deleteNotification = () => {
+        let fade = setInterval(
+            ()=>{
+                console.log(this.element.style.opacity);
+                if (this.element.style.opacity <= 0.05) {
+                    this.element.remove();
+                    clearInterval(fade);
+                } else {
+                    this.element.style.opacity -= 0.05;
+                }
+            },
+            50
+        )
+        fade;
     }
 
     HTMLcomponent = () => {
@@ -23,25 +39,32 @@ export class Notification {
         /*set div style*/
         switch(this.type){
             case "error":
-                container.style.backgroundColor = 'red';
-                container.style.color = 'white';
+                container.style.backgroundColor = '#ff4122';
+                container.style.borderColor = '#c61a09';
+                container.style.color = '#ffc9bb';
                 break;
             case "success":
-                container.style.backgroundColor = 'green';
-                container.style.color = 'white';
+                container.style.backgroundColor = '#87ab69';
+                container.style.borderColor = '#4b6043';
+                container.style.color = '#ddead1';
                 break;
             case "information":
-                container.style.backgroundColor = 'blue';
-                container.style.color = 'white';
+                container.style.backgroundColor = '#1663be';
+                container.style.borderColor = '#104a8e';
+                container.style.color = '#bbd2ec';
                 break;
             default:
                 container.style.backgroundColor = 'black';
+                container.style.borderColor = 'gray';
                 container.style.color = 'white';
                 break;
-
         }
+        container.style.borderRadius = '0 1rem 1rem 0';
+        container.style.borderWidth = '0 0 0 0.25rem'
+        container.style.borderStyle = 'solid';
         container.style.margin = '1vh';
         container.style.padding = '1rem';
+        container.style.opacity = '1';
         return container;
     };
 }
