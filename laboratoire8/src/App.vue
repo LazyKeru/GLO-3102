8a936685a727f16f34102756b2eefee4c736fc34
Alexis Aoun <alexis.aoun@hotmail.com>
2022-11-25 09:13:33 +0100

    frontend WIP

---
+++ laboratoire8/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div></div>
+</template>
+<script>
+import Cookies from 'js-cookie'
+
+export default {
+  data: {
+    loggedIn: false,
+    user: {
+      username: null,
+      password: null,
+      apiKey: null
+    }
+  },
+  
+  methods: {
+    init(){
+      const apiKey = Cookies.get("apiKey")
+      if(apiKey) {
+        this.user = fetchUser()
+
+
+        
+      }
+    }
+    
+  },  
+
+  beforeMount() {
+    init()
+  } ,
+}
+
+</script>
